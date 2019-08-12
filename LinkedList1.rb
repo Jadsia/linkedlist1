f21a3cf57d873841981601582306e655c201bd45
Jadsia <megankyoung@live.com>
2019-08-12 16:56:44 -0700

    added more structure

--- LinkedList1.rb
+++ LinkedList1.rb
@@ -17,8 +17,65 @@ ef print_values(list_node)
   end
 end
 
+class Stack
+  attr_reader :data
+
+  def initialize
+      @data = nil
+  end
+
+  # Push a value onto the stack
+  def push(value)
+      # IMPLEMENT ME!
+  end
+
+
 node1 = LinkedListNode.new(37)
 node2 = LinkedListNode.new(99, node1)
 node3 = LinkedListNode.new(12, node2)
 
 print_values(node3)
+
+
+  # Pop an item off the stack.
+  # Remove the last item that was pushed onto the
+  # stack and return the value to the user
+  def pop
+      # I RETURN A VALUE
+  end
+
+end
+
+def reverse_list(list)
+  # ADD CODE HERE
+
+  while list
+      # ADD CODE HERE
+      list.value
+      list = list.next_node
+  end
+
+  # ADD CODE HERE
+end
+
+# Creates a new Stack object
+stack = Stack.new
+
+# Pushes (adds) the number 1 to the empty stack
+stack.push(1)
+
+# Pushes the number 2 to the TOP of the stack
+stack.push(2)
+
+# Pops (removes) the TOP number from the stack (2)
+puts stack.pop
+=> 2
+
+# Pops the remaining number from the stack (1)
+puts stack.pop
+=> 1
+
+# Tries to pop the TOP number from the stack,
+# but it is empty so it returns nil
+puts stack.pop
+=> nil
