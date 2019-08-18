ea5a69f5210065f5d02faf15540243ca4283f2b8
Jadsia <megankyoung@live.com>
2019-08-18 09:49:51 -0700

    updated

--- LinkedList1.rb
+++ LinkedList1.rb
@@ -5,9 +5,9 @@ class LinkedListNode
     @value = value
     @next_node = next_node
   end
-end
+ end
 
-ef print_values(list_node)
+ def print_values(list_node)
   if list_node
     print "#{list_node.value} --> "
     print_values(list_node.next_node)
@@ -15,67 +15,67 @@ ef print_values(list_node)
     print "nil\n"
     return
   end
-end
+ end
 
-class Stack
+ node1 = LinkedListNode.new(37)
+ node2 = LinkedListNode.new(99, node1)
+ node3 = LinkedListNode.new(12, node2)
+
+ class Stack
   attr_reader :data
 
   def initialize
-      @data = nil
+  @data = nil
   end
 
-  # Push a value onto the stack
-  def push(value)
-      # IMPLEMENT ME!
+  def push(value) @data = LinkedListNode.new(value, @data)
+  end
+   def pop
+  return print "nil\n" if @data.nil?
+  print "#{@data.value}\n"
+  @data = @data.next_node
+  end
   end
 
-
-node1 = LinkedListNode.new(37)
-node2 = LinkedListNode.new(99, node1)
-node3 = LinkedListNode.new(12, node2)
-
-print_values(node3)
-
-
-  # Pop an item off the stack.
-  # Remove the last item that was pushed onto the
-  # stack and return the value to the user
-  def pop
-      # I RETURN A VALUE
-  end
-
-end
-
-def reverse_list(list)
-  # ADD CODE HERE
-
+  def reverse_list(list)
+  stack = Stack.new
   while list
-      # ADD CODE HERE
-      list.value
-      list = list.next_node
+  stack.push(list.value)
+  list = list.next_node
   end
 
-  # ADD CODE HERE
-end
+  return stack.data
+  end
 
-# Creates a new Stack object
-stack = Stack.new
+  print_values(node3)
+  puts "---------"
+ revlist = reverse_list(node3)
+ print_values(revlist)
 
-# Pushes (adds) the number 1 to the empty stack
-stack.push(1)
 
-# Pushes the number 2 to the TOP of the stack
-stack.push(2)
 
-# Pops (removes) the TOP number from the stack (2)
-puts stack.pop
-=> 2
 
-# Pops the remaining number from the stack (1)
-puts stack.pop
-=> 1
+ #notes
+ # Creates a new Stack object
+ # stack = Stack.new
 
-# Tries to pop the TOP number from the stack,
-# but it is empty so it returns nil
-puts stack.pop
-=> nil
+ # Pushes (adds) the number 1 to the empty stack
+ # stack.push(1)
+
+ # Pushes the number 2 to the TOP of the stack
+ # stack.push(2)
+
+ # Pops (removes) the TOP number from the stack (2)
+ # puts stack.pop
+ # => 2
+
+ # Pops the remaining number from the stack (1)
+ # puts stack.pop
+ # => 1
+
+ # Tries to pop the TOP number from the stack,
+ # but it is empty so it returns nil
+ # puts stack.pop
+ # => nil
+
+
